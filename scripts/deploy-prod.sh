b5591f4374f063fbac8b7a3c393255d1d49d5e65
JERRY <hwjameshw@gmail.com>
2023-02-25 13:33:31 +0900

    chore: fastlane 추가

---
+++ scripts/deploy-prod.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+read -p "Version Up (y,n): " versionUp
+
+if [ $versionUp = "y" ]
+then
+    sh ./scripts/version-up.sh
+else
+    sh ./scripts/clean.sh
+fi
+
+# Deploy android 
+cd android
+fastlane fb_prod
+
+# Deploy ios 
+cd ../ios
+fastlane fb_prod
