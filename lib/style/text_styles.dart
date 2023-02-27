377ab904be99c6513395cecf4c04fb551ef20bae
JERRY <hwjameshw@gmail.com>
2023-02-27 13:08:07 +0900

    feat: 폰트 추가

---
+++ lib/style/text_styles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  TextStyles._();
+
+  static const thin = TextStyle(
+    fontFamily: "SpoqaHanSansNeo",
+    fontWeight: FontWeight.w100,
+  );
+
+  static const light = TextStyle(
+    fontFamily: "SpoqaHanSansNeo",
+    fontWeight: FontWeight.w300,
+  );
+
+  static const regular = TextStyle(
+    fontFamily: "SpoqaHanSansNeo",
+    fontWeight: FontWeight.w400,
+  );
+
+  static const medium = TextStyle(
+    fontFamily: "SpoqaHanSansNeo",
+    fontWeight: FontWeight.w500,
+  );
+
+  static const bold = TextStyle(
+    fontFamily: "SpoqaHanSansNeo",
+    fontWeight: FontWeight.w700,
+  );
+}
