4badaf03cf9b896dfd53ad86d0bcdeb25fdc8d8d
joyoon <jhyry@naver.com>
2023-03-02 08:16:20 +0900

    feat: 비밀번호 찾기 화면 수정

--- lib/module/pwfind/pwfind_view.dart
+++ lib/module/pwfind/pwfind_view.dart
@@ -1,11 +1,180 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ohsundosun/style/color_style.dart';
+import 'package:ohsundosun/widget/appbar.dart';
+import 'package:ohsundosun/widget/safebox.dart';
 
 class PwfindView extends ConsumerWidget {
   const PwfindView({super.key});
 
   @override
   Widget build(BuildContext context, ref) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeBox(
+        top: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const BackAppBar(),
+            const SizedBox(
+              height: 27,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: const Text(
+                "이메일 아이디를 입력하여 \n임시 비밀번호로 로그인합니다.",
+                style: TextStyle(
+                  color: ColorStyle.black100,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: -1,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 95,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: const Text(
+                "이메일 아이디",
+                style: TextStyle(
+                  color: ColorStyle.black100,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: -1,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 7,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 10, left: 10),
+                    height: 60,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: ColorStyle.black20,
+                        width: 1,
+                      ),
+                      color: ColorStyle.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: '가입한 이메일을 입력하세요.',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: ColorStyle.red100,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "인증 발송",
+                        style: TextStyle(
+                          color: ColorStyle.white,
+                          fontSize: 19,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: const Text(
+                "임시 비밀번호",
+                style: TextStyle(
+                  color: ColorStyle.black100,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: -1,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 7,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 10, left: 10),
+                    height: 60,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: ColorStyle.black20,
+                        width: 1,
+                      ),
+                      color: ColorStyle.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: '받으신 임시 비밀번호를 입력하세요.',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: ColorStyle.black100,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "로그인",
+                        style: TextStyle(
+                          color: ColorStyle.white,
+                          fontSize: 19,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
