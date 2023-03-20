0fe8895a63189447709ded8d21d5c7b353a84399
JERRY <hwjameshw@gmail.com>
2023-03-20 11:06:00 +0900

    feat: 회원가입 화면 텍스트 수정

--- lib/module/onboard/sign_up/sign_up_view.dart
+++ lib/module/onboard/sign_up/sign_up_view.dart
@@ -21,7 +21,7 @@ class SignUpView extends ConsumerWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "회원가입",
+                    "                      회원가입",
                     style: SpoqaHanSansNeo.bold.set(
                       size: 28,
                       height: 35,
@@ -30,12 +30,54 @@ class SignUpView extends ConsumerWidget {
                     ),
                   ),
                   ODHeight(80),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          "계정정보",
+                          style: SpoqaHanSansNeo.medium.set(
+                            size: 16,
+                            height: 35,
+                            letter: -1,
+                            color: ColorStyles.black100,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          "유형선택",
+                          style: SpoqaHanSansNeo.medium.set(
+                            size: 16,
+                            height: 35,
+                            letter: -1,
+                            color: ColorStyles.black100,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          "완료",
+                          style: SpoqaHanSansNeo.medium.set(
+                            size: 16,
+                            height: 35,
+                            letter: -1,
+                            color: ColorStyles.black100,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  ODHeight(7),
                   const LinearProgressIndicator(
+                    minHeight: 8,
                     value: 0.33,
                     color: ColorStyles.red100,
                     backgroundColor: ColorStyles.black10,
                   ),
-                  ODHeight(25),
+                  ODHeight(22),
                   Text(
                     "닉네임",
                     style: SpoqaHanSansNeo.medium.set(
@@ -48,7 +90,7 @@ class SignUpView extends ConsumerWidget {
                   const ODInput(
                     hintText: "한글, 영어 최대 8자",
                   ),
-                  ODHeight(15),
+                  ODHeight(12),
                   Text(
                     "이메일 아이디",
                     style: SpoqaHanSansNeo.medium.set(
@@ -87,7 +129,7 @@ class SignUpView extends ConsumerWidget {
                       ),
                     ],
                   ),
-                  ODHeight(15),
+                  ODHeight(12),
                   Text(
                     "비밀번호",
                     style: SpoqaHanSansNeo.medium.set(
@@ -115,7 +157,7 @@ class SignUpView extends ConsumerWidget {
                     obscureText: true,
                     hintText: "영문+숫자 조합 8~16자",
                   ),
-                  ODHeight(75),
+                  ODHeight(45),
                   const ODButton("다음 단계로"),
                 ],
               ),
